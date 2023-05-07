cf9f9fdf694bd1bf0e160853fdc6c6d586c0a33a
Yiheng Xu <yhxu@umd.edu>
2023-05-07 10:09:48 -0400

    revert enemy shooter. fix enemy shooter issue

--- Assets/code/EnemyShooter.cs
+++ Assets/code/EnemyShooter.cs
@@ -2,55 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/* ToDo: 
-    In the scene, open a big window on the cafe wall and make a small room, put the robot Peter modeled there.
-    Add a gun to the robot.
-    Currently this script is added to the wall. Once added that, we should put the script to the robot.
-    
-    Besides, add red boarders on the ground to tell player in this region the hero will be shot.
-    The region is a 100 x 25 rectangle next to the wall.
-*/
-
 public class EnemyShooter : MonoBehaviour
 {
-    // public Hero hero;
-    // public float startTime;
-    // public Rigidbody bullet;
-    // public float bulletSpeed = 15;
-
-    // AudioSource shootingSound;
-
-    // void Start()
-    // {
-    //     Vector3 heroPreviousPostion;
-    //     gameObject.GetComponent<EnemyShooter>().enabled = false;
-    //     shootingSound = GetComponent<AudioSource>();
-
-    // }
-
-    // void Update()
-    // {
-    //     if (gameObject.GetComponent<EnemyShooter>().enabled && Time.time - startTime >= Random.Range(0.8f, 1.5f)) {
-    //         transform.forward = hero.transform.position - transform.position;
-    //         Shoot();
-    //         startTime = Time.time;
-    //     }
-    // }
-
-    // void Shoot()
-    // {
-    //     shootingSound.Play();
-    //     Vector3 spawnPoint = transform.position;
-    //     Rigidbody p = Instantiate(bullet, spawnPoint + transform.forward, Quaternion.Euler(0, 0, 0), gameObject.GetComponent<EnemyShooter>().transform.parent);
-    //     p.velocity = transform.forward * bulletSpeed;
-    // }
     public Hero hero;
     public float startTime;
     AudioSource shootingSound;
-
-    public Rigidbody enemyBullet;
-    public float bulletSpeed = 100;
-
 
     void Start()
     {
@@ -71,12 +27,8 @@ public class EnemyShooter : MonoBehaviour
         // print("shoot");
         shootingSound.Play();
         
-        // if (!hero.isCovered) {
-        //     hero.isDead = true; 
-        // }
-        Rigidbody p = Instantiate(enemyBullet, transform.position + transform.forward, transform.rotation);
-        p.velocity = transform.forward * bulletSpeed;
-
+        if (!hero.isCovered) {
+            hero.isDead = true; 
+        }
     }
-
 }
